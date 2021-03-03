e82599620506ee845fb5afafab8edc60ec1fa1b5
Ian Matthew Morey <ian.matthew.morey@gmail.com>
2021-03-03 02:42:48 -0500

    more updates!

---
+++ js/script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+
+
+$(".siteplan").click(function(){
+    $('#change-image').attr("src", "images/Site_Plan_20210302.png")
+});
+
+$(".sectioncut").click(function(){
+    $('#change-image').attr("src", "images/Section_Cut_EW_FacingNorth_20210303.png")
+});
+ 
+
+});

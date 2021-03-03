e784bee9d7866e5797cc2a5ab1a1b39c7437f5dc
Ian Matthew Morey <ian.matthew.morey@gmail.com>
2021-03-03 12:37:00 -0500

    active selectors
    played around with changing text decoration based on active drawing

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-jQuery(document).ready(function($){
+$(document).ready(function($){
 
 
-$(".siteplan").click(function(){
-    $('#change-image').attr("src", "images/Site_Plan_20210302.png")
+$("#siteplan-click").click(function(){
+    $('#change-image').attr('src', 'images/Site_Plan_20210302.png');
+    $('#change-image').attr('class', 'siteplan');
 });
 
-$(".sectioncut").click(function(){
-    $('#change-image').attr("src", "images/Section_Cut_EW_FacingNorth_20210303.png")
+$("#sectioncut-click").click(function(){
+    $('#change-image').attr('src', 'images/Section_Cut_EW_FacingNorth_20210303.png');
+    $('#change-image').attr('class', 'sectioncut');
 });
- 
+
+$("span").click(function(){
+    $('span').removeClass('active');
+    $(this).addClass('active');
+});
+
+
 
 });
